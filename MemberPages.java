b4240fdc1303c0dc8350f878df41aae1df29ab20
Zach <Zar398@txstate.edu>
2022-07-25 08:45:59 -0500

    added Member info dump method

--- MemberPages.java
+++ MemberPages.java
@@ -10,11 +10,21 @@ public class MemberPages {
         newMember.setName(name);
         newMember.setphoneNum(phoneNum);
     }
+    public void dumpMember(Member currentMember){
+        System.out.println(currentMember.getName());
+        System.out.println(currentMember.getId());
+        System.out.println(currentMember.getPhoneNum());
+        System.out.println(currentMember.getTickets());
+        System.out.println(currentMember.getOverdue());
+        System.out.println(currentMember.getFineTotal());
 
+    }
     public static void main(String[] args) {
 
         MemberPages yellow = new MemberPages();
         Member newMember = new Member();
         yellow.addMemeber(newMember, "Kelly Marx", "51SD226", "512-999-9999");
+        yellow.dumpMember(newMember);
+    
     }
 }
