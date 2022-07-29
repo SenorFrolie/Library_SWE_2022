56db99aba6684ca4082edaf1a2c7f8a7b119e383
Bilal Hjiouaj <bsh52@txstate.edu>
2022-07-28 23:32:52 -0500

    added ledger checkout functionality

--- Ledger.java
+++ Ledger.java
@@ -1,23 +1,35 @@
-//import java.io.IOException;
+import java.io.IOException;
 import java.util.*; 
+import java.io.FileWriter;
+
+import com.opencsv.CSVWriter;
 
 public class Ledger {
 
-    private static HashMap<Integer,ArrayList<String>> transactions = new HashMap<Integer,ArrayList<String>>();
+    //private static HashMap<Integer,ArrayList<String>> transactions = new HashMap<Integer,ArrayList<String>>();
+    ArrayList<String> userInfo = new ArrayList<>();
 
-    public static ArrayList<String> getLedger(Integer libID) {
+    /*public static ArrayList<String> getLedger(Integer libID) {
         return transactions.get(libID);
-    }
+    }*/
 
-    public static void setLedger(String name, String address, String phoneNum, Integer libID, String tickets) {
-        ArrayList<String> userInfo = new ArrayList<>();
-        userInfo.add(name);
-        userInfo.add(address);
-        userInfo.add(phoneNum);
-        userInfo.add(tickets);
-        transactions.put(libID, userInfo);
+    public void setLedger(String libID, String bookIDs) throws IOException{
+        String csv = "Ledger.csv";
+        CSVWriter writer = new CSVWriter(new FileWriter(csv, true));
+        try {
+            writer.writeNext(new String[]{libID, bookIDs});
+            System.out.println("You have successfully checked out: " + bookIDs);
+        } catch (Exception e) {
+            System.out.println("Failed to checkout books: "+bookIDs);
+        } finally {
+            writer.close();
+        }
+        //ArrayList<String> userInfo = new ArrayList<>();
+        //transactions.put(libID, userInfo);
     }
-    //public static void main(String[] args) throws IOException {
-    //    System.out.println("Where would you like to go? ");
-    //}
+    public static void main(String[] args) throws IOException {
+        //Ledger ledger = new Ledger();
+        //ledger.setLedger(1,1);
+        //System.out.println("Where would you like to go? ");
+    }
 }
