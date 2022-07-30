eb4f091e9214a19b2c888b135bff2ecbbef0c289
Ertugrul Oksuz <e_o133@txstate.edu>
2022-07-30 15:45:58 -0500

    Checkout Class added

--- Checkout.java
+++ Checkout.java
@@ -1,35 +1,146 @@
+import java.io.*;
+//import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Scanner;
+import java.util.concurrent.TimeoutException;
+import com.opencsv.*;
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
+
+
 public class Checkout{
-    private String UserName; //name of user or library id for user
-    private String BookName; //name of the book being checked out
-    private String VisualName; //name of the audio/video being checked out
-    private String CheckoutDate; //date item is being checked out
-    private String DueDate; //due date for the item
+    // would you like to checkout book or visual material?
+    public void startCheckOut(){
+        
+
+        do{
+            System.out.println("\nWould you like to checkout a book or visual material?");
+            System.out.println("1. Book");
+            System.out.println("2. Visual Material");
+
+            Scanner sc = new Scanner(System.in);
+            int user_input;
+
+            user_input = sc.nextInt();
+
+            if(user_input == 1){
+                System.out.println("Please enter book ID");
+                Scanner bookIdInput = new Scanner(System.in);
+
+                String bookidNum = bookIdInput.nextLine();
+                String availabality = BookShelf.findID(bookidNum);
+                if(availabality == "Available"){
+                bookCheckOut(bookidNum);
+                }
+                else if(availabality != "Available"){
+                    System.out.println("Book is not available");
+                    //request option from ...
+                }
+            }
+            if(user_input == 2){
+                System.out.println("Please enter visual ID");
+                Scanner visualIdInput = new Scanner(System.in);
+
+                String bookidNum = visualIdInput.nextLine();
+                String availabality = BookShelf.findID(bookidNum);
+                if(availabality == "Available"){
+                visualCheckOut(bookidNum);
+                }
+                else if(availabality != "Available"){
+                    System.out.println("Visual Material is not available");
+                    //request option from ...
+                }
+            }
+        }
+        
+
+    }
+
+//book checkout process function
+//which will check book availability and if not available request item
+//if available get due date for book with book id from ledger
+//assign book to user with ledger
+//print reciept to user to show due date of book
 
 
-    public Checkout(String UserName, String BookName, String VisualName, String CheckoutDate, String DueDate){
-        this.UserName = UserName;
-        this.BookName = BookName;
-        this.VisualName = VisualName;
-        this.CheckoutDate = CheckoutDate;
-        this.DueDate = DueDate;
-    }
 
-    public String getUserName(){
-        return UserName;
-    }
-    public String getBookName(){
-        return BookName;
-    }
-    public String getVisualName(){
-        return VisualName;
-    }
-    public String getCheckoutDate(){
-        return CheckoutDate;
-    }
-    public String getDueDate(){
-        return DueDate;
-    }
-
+//visual material checkout process function
+//which will check availability and if not available request item
+//if available get due date for visual mat with visual mat id from ledger
+//assign visual mat to user with ledger
+//print reciept to user to show due date of visual mat
 }
 
 
+public void bookCheckOut(String bookId){
+
+    Scanner sc = new Scanner(System.in);
+    
+    //Check if available
+
+    do{
+        System.out.println("\nWould you like to checkout: " + bookId);
+        System.out.println("1. Checkout");
+        System.out.println("2. Look for different option (back)");
+        System.out.println("3. Log Out");
+
+        int user_input = sc.nextInt();
+        
+        if(user_input == 1){
+            //assign book to user with ledger
+            //print to confirm
+            //ask next action
+        }
+        else if(user_input == 2){
+            startCheckOut();
+        }
+        else if(user_input == 3){
+            String[] args;
+            TheSystem.main(args);
+        }
+        else{
+            System.out.println("please choose one of the options");
+        }
+    while(user_input != 0){
+
+    }
+
+}
+}
+
+public void visualCheckOut(String visualID){
+
+    Scanner sc = new Scanner(System.in);
+    
+    //Check if available
+
+    do{
+        System.out.println("\nWould you like to checkout: " + visualID);
+        System.out.println("1. Checkout");
+        System.out.println("2. Look for different option (back)");
+        System.out.println("3. Log Out");
+
+        int user_input = sc.nextInt();
+        
+        if(user_input == 1){
+            //assign visual to user with ledger
+            //print to confirm
+            //ask next action
+        }
+        else if(user_input == 2){
+            startCheckOut();
+        }
+        else if(user_input == 3){
+            String[] args;
+            TheSystem.main(args);
+        }
+        else{
+            System.out.println("please choose one of the options");
+        }
+    while(user_input != 0){
+
+    }
+}
+}
