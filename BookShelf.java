6fb3b5f01dfda0546b8a194f0a949beedf22333c
Robert Bonham <reb159@txstate.edu>
2022-08-01 23:03:59 -0500

    Updated Bookshelf methods

--- BookShelf.java
+++ BookShelf.java
@@ -1,5 +1,4 @@
 import java.io.*;
-//import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Scanner;
 import com.opencsv.*;
@@ -47,14 +46,17 @@ public class BookShelf {
 
     
     // Procesing Visual.csv into objects 
-    public void readVideoMat(){
+    public void readVideoMat() throws IOException{
+/*
         try {
+            String line =null;
             FileReader frV = new FileReader("Visual.csv");
-            BufferedReader brV = new BufferedReader(frV);
-            String stringReadV = brV.readLine();
+            BufferedReader br = new BufferedReader(frV);
+            br.readLine();
+           // String stringReadV = br.readLine();
 
-            while (stringReadV != null) {
-                String[] elementsV = stringReadV.split(",");
+            while ((line = br.readLine()) != null) {
+                String[] elementsV = line.split(",");
 
                 if (elementsV.length < 3) {
                     throw new RuntimeException("line too short"); // handle missing entries
@@ -69,12 +71,45 @@ public class BookShelf {
                 dataList_VS.add((VisualMat) temp_2);
                 
                 // read the next line
-                stringReadV = brV.readLine();
-                brV.close();
+                line = br.readLine();
                 }
+                br.close();
+
         } catch (Exception e) {
             System.out.println("ERROR: Invalid Visual read CSV file read. . .");
         }
+        */
+
+
+        FileReader fr = new FileReader("Visual.csv");
+        BufferedReader br = new BufferedReader(fr);
+        // Processing Books.CSV into objects 
+        try {
+            br.readLine(); // read first line to avoid displaying file header
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                String[] elements = line.split(",");
+
+                if (elements.length < 3) {
+                    throw new RuntimeException("line too short"); // handle missing entries
+                }
+                
+                String Title = elements[0];
+                String Director = elements[1];
+                String Id = elements[2];
+
+                VisualMat temp_2 = new VisualMat(Title, Director, Id);
+                dataList_VS.add((VisualMat) temp_2);
+
+            }
+            br.close();
+
+        } catch (Exception e) {
+            System.out.println("ERROR: Invalid books CSV file read. . .");
+        } finally {
+            br.close();
+        }
+        
 }
 
 
@@ -82,7 +117,6 @@ public class BookShelf {
     public static void ShelfMainMenu() throws IOException{
         Scanner sc = new Scanner(System.in);
        // BookShelf books = new BookShelf();
-
         System.out.println("--------------------------------------");
         System.out.println("| Welcome to our hottest Selection! | ");
         System.out.println("--------------------------------------");
@@ -92,9 +126,8 @@ public class BookShelf {
                 System.out.println("\r\n");
                 System.out.println("1. Books ");
                 System.out.println("2. Visual ");
-                System.out.println("3. Return to Main Menu ");
-                System.out.println("4. Request new book ");
-                System.out.println("0. Kill Program ");
+                System.out.println("3. Request new book  ");
+                System.out.println("4. Return to Main Menu ");
 
                 int user_input = sc.nextInt();
 
@@ -106,7 +139,7 @@ public class BookShelf {
                 for(Books book:dataList){
                     System.out.println(book.getTitle() + " by " + book.getAuthor());
                 }
-                FilterBooks();
+                //FilterBooks();
                 //This is where checkouts goes
              
             }
@@ -117,22 +150,20 @@ public class BookShelf {
                 for(VisualMat book :dataList_VS){
                     System.out.println(book.getTitle() + " by " + book.getDirector());
                 }
-                
+ 
             }
             else if(user_input == 3){
-                sc.close();
-                return;
-
+                requestNewBook();
+                TheSystem.mainMenu();
             }
             else if(user_input == 4){
-                requestNewBook();
+                TheSystem.mainMenu();
             }
-            sc.close();
-
-        //} while (user_input != 0);
+            //sc.close();
     }
 
     
+//=============================================
 
     static public void FilterBooks(){
         Scanner sc = new Scanner(System.in);
@@ -153,36 +184,38 @@ public class BookShelf {
             System.out.println("2: By Author ");
             System.out.println("3: By Best Seller ");
 
-        int filter =sc.nextInt();
+            int filter =sc.nextInt();
 
-        if(filter == 1){
-            System.out.println("---------------------------");
-                
-                for (Books items : dataList) {
-                    System.out.println(items.getTitle());
-                }
-            System.out.println("---------------------------");
-
-            }else if(filter == 2){
-            System.out.println("---------------------------");
-                for (Books items : dataList) {
-                    System.out.println(items.getAuthor());
-                }
-            System.out.println("---------------------------");
-            }
-            else if(filter == 3){
-            System.out.println("---------------------------");
-                for (Books items : dataList) {
-                    if(items.getBOTY().equals("Y")){
-                        System.out.println(items.getTitle() + " by "+ items.getAuthor());
+            if(filter == 1){
+                System.out.println("---------------------------");
+                    
+                    for (Books items : dataList) {
+                        System.out.println(items.getTitle());
                     }
+                System.out.println("---------------------------");
+
+                }else if(filter == 2){
+                System.out.println("---------------------------");
+                    for (Books items : dataList) {
+                        System.out.println(items.getAuthor());
+                    }
+                System.out.println("---------------------------");
                 }
-            System.out.println("---------------------------");
+                else if(filter == 3){
+                System.out.println("---------------------------");
+                    for (Books items : dataList) {
+                        if(items.getBOTY().equals("Y")){
+                            System.out.println(items.getTitle() + " by "+ items.getAuthor());
+                        }
+                    }
+                System.out.println("---------------------------");
             }
         }
         sc.close();
     }
+//=============================================
 
+//=============================================
     
         public static String findID(String ID){
             for(int i = 0; i < dataList.size();i++){
@@ -203,6 +236,7 @@ public class BookShelf {
             }
             return "not Found";
         }
+//=============================================
 
         public String findBookByID(String ID){
             for(int i = 0; i < dataList.size();i++){
@@ -235,6 +269,7 @@ public class BookShelf {
             }
             return "not Found";
         }
+//=============================================
 
         public String findVMByID(String ID){
             for(int i = 0; i < dataList_VS.size();i++){
@@ -244,7 +279,18 @@ public class BookShelf {
             }
             return "not Found";
         }
-
+//=============================================
+              
+        public static int findQuantity(String ID){
+            for(int i = 0; i < dataList_VS.size();i++){
+               if(dataList_VS.get(i).getRefID().equals(ID)){
+                   int J = Integer.parseInt(dataList.get(i).getQuantiy());
+                   return J;
+               }
+            }
+            return 0;
+        }
+//=============================================
 
 
 
@@ -267,8 +313,10 @@ public class BookShelf {
             }
             return "Not Found";  
         }
+//=============================================
 
 
+//=============================================
         public boolean isBestSeller(String ID){
 
             for(int i = 0; i < dataList.size();i++){
@@ -279,8 +327,10 @@ public class BookShelf {
            }
            return false;  
         }
+//=============================================
 
         public static void requestNewBook() throws IOException{
+            //TheSystem Ui = new TheSystem();
             System.out.println("---------------------------------------");
             System.out.println("Is this a Book or Visual Material? ");
             System.out.println("1: Book ");
@@ -290,40 +340,50 @@ public class BookShelf {
             Scanner sc = new Scanner(System.in);
             int Answer = sc.nextInt();
             if(Answer == 1){
-                String booksrc = "Books.csv";
+                Scanner scan = new Scanner(System.in);
+                String booksrc = "requestMaterial.csv";
                 System.out.println("Please enter the necessary infomation. ");
-                System.out.println("For example: Title, Author ");
+                //System.out.println("For example: Title, Author ");
+                System.out.print("Title: ");
+                String Title = scan.nextLine();
+                System.out.print("Author: ");
+                String Author = scan.nextLine();
 
-                System.out.print("Title and Author: " );
-                String Title = sc.next();
-                String requested = Title + " (requested)";
-                //CSVWriter writer = new CSVWriter(new FileWriter(booksrc, true));
+
+                String requested = Title + ", " + Author + " (requested)" ;
                 CSVWriter writer = new CSVWriter(new FileWriter(booksrc, true), ',',
                                              CSVWriter.NO_QUOTE_CHARACTER,
                                              CSVWriter.DEFAULT_ESCAPE_CHARACTER,
                                              CSVWriter.DEFAULT_LINE_END);
                
-               
                 String [] record = requested.split(",");
                 writer.writeNext(record);
-
+                scan.close();
                 writer.close();
             }
             else{
-                String Visualsrc = "Visual.csv";
+                Scanner scan = new Scanner(System.in);
+                String Visualsrc = "requestMaterial.csv";
                 System.out.println("Please enter the necessary infomation. ");
                 System.out.println("---------------------------------------");
 
-                System.out.println("Title and Director: " );
-                String Title = sc.next();
-                String requested = Title + " (requested)";
+                System.out.print("Title: ");
+                String Title = scan.nextLine();
+                System.out.print("Author: ");
+                String Director = scan.nextLine();
 
 
-                CSVWriter writer = new CSVWriter(new FileWriter(Visualsrc, true));
+                String requested = Title + ", " + Director + " (requested)" ;
+
+                CSVWriter writer = new CSVWriter(new FileWriter(Visualsrc, true), ',',
+                                                            CSVWriter.NO_QUOTE_CHARACTER,
+                                                            CSVWriter.DEFAULT_ESCAPE_CHARACTER,
+                                                            CSVWriter.DEFAULT_LINE_END);                      
                 String [] record = requested.split(",");
                 writer.writeNext(record);
                 writer.close();
                 sc.close();
+                scan.close();
             }
         }
 
@@ -343,6 +403,7 @@ public class BookShelf {
                int index = findMemberIndex(ID);
                 return dataList.get(index);
             }
+
 
     public int findMemberIndex (String ID){
             int index= 0;
@@ -367,35 +428,6 @@ public class BookShelf {
         }
         return 0;
     }
-
-        
-/*
-        public static void updateCSV(String fileToUpdate, String new_Quant_value, String old_Quant_value ,String BookTitle) throws IOException, CsvException {
-
-                    File inputFile = new File("Books.csv");
-
-                    // Read existing file
-                    CSVReader reader = new CSVReader(new FileReader(fileToUpdate));
-                    List<String[]> csvBody = reader.readAll();
-                    // get CSV row column and replace with by using row and column
-
-                    for(int i = 0;i < dataList.size();i++){
-                        for(int j = 0; j < dataList.size();j++){
-                            if(dataList.get(i).getTitle().equals(BookTitle) && dataList.get(j).getQuantiy().equals(old_Quant_value)){
-                                csvBody.get(i)[j] = new_Quant_value;
-                                reader.close();
-                            } 
-                        }
-                    }
-
-            
-                    // Write to CSV file which is open
-                    CSVWriter writer = new CSVWriter(new FileWriter(inputFile));
-                    writer.writeAll(csvBody);
-                    writer.flush();
-                    writer.close();
-        }
-*/
 
 
 public static void main(String[] args) throws IOException, CsvException {
